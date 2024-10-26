c14fec39cd5eb5f39e0d008c4c95d64f2f4a0ba5
Hasiyamb <Ssanuwa624@gmail.com>
2024-10-26 16:00:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID, "DOxG2CaC#B5fa_EXVTIpKhwkpiwKy8LO8oqcrpURrUyJe42ibeF0"
 MONGODB: process.env.MONGODB || "enter mongodb here",
 };
